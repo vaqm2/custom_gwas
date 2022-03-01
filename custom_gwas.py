0cf9d465de3af30b1e30a956c737f125020c9c11
vaqm2 <appadurai.vivek@gmail.com>
2022-03-01 15:38:59 +0100

    Updated skeleton

--- custom_gwas.py
+++ custom_gwas.py
@@ -2,6 +2,7 @@
 
 import sys
 import argparse
+from tarfile import PAX_NAME_FIELDS
 from cyvcf2 import VCF
 from os import path
 import numpy as np
@@ -17,15 +18,30 @@ parser.add_argument('--out', type = str, help = "Output prefix", required = True
 np.set_printoptions(precision = 3)
 args = parser.parse_args()
 
+if path.exists(args.pheno):
+    pheno_df = pd.read_csv(args.pheno, sep = ' ')
+else:
+    print('ERROR: Unable to read PHENOTYPE file: ')
+    print(args.pheno)
+    sys.exit()
+
+if path.exists(args.covar):
+    covar_df = pd.read_csv(args.covar, sep = ' ')
+else:
+    print('ERROR: Unable to read COVARIATE file: ')
+    print(args.covar)
+    sys.exit()
+
+pheno_cov_df = pd.merge(pheno_df, covar_df, on = 'IID')
+
 if path.exists(args.vcf):
     vcf_file = VCF(args.vcf)
-    samples = vcf_file.samples
+    samples_df = pd.DataFrame(vcf_file.samples, columns = ['IID'])
+    
     for variant in vcf_file:
         dosages = variant.format('DS')
-        print(samples)
-        print(dosages.ravel())
-        print(dosages.shape)
-        print(dosages.ndim)
-        print(dosages.size)
-        print(dosages.dtype)
+        dosages_df = pd.DataFrame(dosages, columns = ['DOSAGE'])
+        samples_ds_df = pd.concat([samples_df.reset_index(drop = True), dosages_df.reset_index(drop = True)], axis = 1)
+        to_regress_df = pd.merge(pheno_cov_df, samples_ds_df, on = 'IID')
+        print(to_regress_df.head())
         sys.exit()
