a251cac25b864f8eb816347b2b8e44bd69cb3f96
vaqm2 <appadurai.vivek@gmail.com>
2022-07-06 15:30:33 +0200

    reduce update frequency

--- bin/custom_gwas.py
+++ bin/custom_gwas.py
@@ -146,7 +146,7 @@ def main():
                     outputBuffer += N + "\n"
                     linesProcessed += 1
 
-                    if(linesProcessed > 0 and linesProcessed % 10000 == 0):
+                    if(linesProcessed > 0 and linesProcessed % 100 == 0):
                         print ("PROGRESS: Processed ", linesProcessed, " ", "variants")
 
             # Empty buffer to output all at once
