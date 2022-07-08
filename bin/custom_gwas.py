fac5350201d8160df462612d0ba221f9be049a27
vaqm2 <appadurai.vivek@gmail.com>
2022-07-08 15:56:23 +0200

    multiprocessing and living dangeroously 8)

--- bin/custom_gwas.py
+++ bin/custom_gwas.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 
-from operator import sub
+from multiprocessing.dummy import freeze_support
 import sys
 import argparse
+import multiprocessing as mp
+import itertools as its
 from cyvcf2 import VCF
-from os import path
+from sgkit.io.vcf import partition_into_regions
 import numpy as np
 import pandas as pd
 import rpy2.robjects as robjects
 from rpy2.robjects import pandas2ri
 from rpy2.robjects.conversion import localconverter
-from datetime import datetime
 
 def main():
     parser = argparse.ArgumentParser(description = "Code to run custom GWAS with VCF files")
@@ -41,6 +42,7 @@ def main():
 
     np.set_printoptions(precision = 4)
     args = parser.parse_args()
+    num_cpus = mp.cpu_count()
 
     try:
         pheno_df = pd.read_csv(args.pheno, sep = "\s+")
@@ -61,103 +63,114 @@ def main():
         covar_df.set_index('IID', inplace = True)
 
     try:
-        robjects.r['source'](args.rscript)
-        model_to_fit = robjects.globalenv[args.model]
-    except:
-        print("ERROR: When opening rscript: ", sys.exc_info()[0], "occurred!")
-        sys.exit()
-
-    try:
         pheno_cov_df = pheno_df.join(covar_df, how = 'inner')
     except:
-        print("ERROR: When intersecting PHENOTYPE & COVARIATE files: ", 
-            sys.exc_info()[0], 
-            "occurred!")
+        print("ERROR: When intersecting PHENOTYPE & COVARIATE files: ", sys.exc_info()[0], "occurred!")
         sys.exit()
 
     try:
-        vcf_file = VCF(args.vcf)
+        regions = partition_into_regions(args.vcf, num_parts = num_cpus)
+        print(regions)
+        num_chunks = len(regions)
     except:
-        print("ERROR: ", sys.exc_info()[0], "occurred!")
+        print("ERROR: When partitioning VCF file ", sys.exc_info()[0], " occurred!")
         sys.exit()
     else:
         try:
             out_fh = open(args.out, "w")
         except:
-            print("ERROR: ", sys.exc_info()[0], "occurred!\n")
+            print("ERROR: When creating output file ", sys.exc_info()[0], " occurred!")
             sys.exit()
         else:
-            vcf_samples_df = pd.DataFrame()
-            vcf_samples_df['IID'] = vcf_file.samples
-            vcf_samples_df.IID = vcf_samples_df.IID.astype(str)
-            vcf_samples_df.set_index('IID', inplace = True)
-            analysis_df = vcf_samples_df.join(pheno_cov_df, how = 'left')
-            out_fh.write("CHR POS SNP REF ALT ESTIMATE SE P N\n")
-            linesProcessed = 0
+            try:
+                vcf_file = VCF(args.vcf)
+            except:
+                print("ERROR: When reading VCF file ", sys.exc_info()[0], " occurred!")
+                sys.exit()
+            else:
+                vcf_samples_df = pd.DataFrame()
+                vcf_samples_df['IID'] = vcf_file.samples
+                vcf_samples_df.IID = vcf_samples_df.IID.astype(str)
+                vcf_samples_df.set_index('IID', inplace = True)
+                analysis_df = vcf_samples_df.join(pheno_cov_df, how = 'left')
+                out_fh.write("CHR POS SNP REF ALT ESTIMATE SE P N\n")
+                vcf_file.close()
 
-            for variant in vcf_file:
-                chromosome = variant.CHROM
-                position = str(variant.start + 1)
-                rsId = variant.ID
-                refAllele = variant.REF
-                altAllele = ''.join(variant.ALT)
-                
-                # Ignore multi-allelic loci
-                if(len(refAllele) > 1 or len(altAllele) > 1):
-                    print("INFO: Skipping INDEL/MULTI-ALLELIC variant: ", 
-                        rsId, " ",
-                        chromosome, " ", 
-                        position, " ", 
-                        refAllele, " ", 
-                        altAllele)
-                    continue
+                # Start multiple processes for different VCF chunk regions
+                p = mp.Pool(num_chunks)
+                model_args = []
+                for i in range(0, num_chunks):
+                    model_args.append((args.vcf, regions[i], analysis_df, pheno_cov_df.columns, args.rscript, args.model))
+                results = p.starmap(run_model, model_args)
+
+                # Write results to output file
+                for chunk_result in results:
+                    out_fh.write(chunk_result)
+                out_fh.close()
+        
+def run_model(vcf_path, region, analysis_df, covariate_names, r_script, r_model):
+    try:
+        robjects.r['source'](r_script)
+    except:
+        print("ERROR: When opening rscript: ", sys.exc_info()[0], " occurred!")
+        sys.exit()
+    else:
+        model_to_fit = robjects.globalenv[r_model]
+
+    try:
+        vcf_file = VCF(vcf_path)
+    except:
+        print("ERROR: When reading VCF file ", sys.exc_info()[0], " occurred!")
+    else:
+        chunk_result = ""
+        for variant in vcf_file(region):
+            chromosome = variant.CHROM
+            position = str(variant.start + 1)
+            rsId = variant.ID
+            refAllele = variant.REF
+            altAllele = ''.join(variant.ALT)
+
+            # Ignore multi-allelic loci
+            if(len(refAllele) > 1 or len(altAllele) > 1):
+                print("INFO: Skipping INDEL/MULTI-ALLELIC variant: ", chromosome, " ", position, " ", refAllele, " ", altAllele)
+                continue
+            else:
+                try:
+                    dosages = variant.format('DS')
+                except:
+                    print("INFO: Missing DS format field at: ", chromosome, " ", position, " ", refAllele, " ", altAllele, " ", "Skipping..")
                 else:
+                    analysis_df['DOSAGE'] = dosages
+                    analysis_df.dropna(subset = covariate_names)
+                    N = str(analysis_df.shape[0])
+
+                    # Convert pandas dataframe to R dataframe
+                    with localconverter(robjects.default_converter + pandas2ri.converter): 
+                        analysis_df_r = robjects.conversion.py2rpy(analysis_df)
+
+                    # Call the user-specified R function to perform the association test
                     try:
-                        dosages = variant.format('DS')
+                        result_df_r = model_to_fit(analysis_df_r)
                     except:
-                        print("INFO: Missing DS format field at: ",
-                            rsId, " ",
-                            chromosome, " ",
-                            position, " ",
-                            refAllele, " ",
-                            altAllele, " ",
-                            "Skipping..")
-                    else:
-                        analysis_df['DOSAGE'] = dosages
-                        analysis_df.dropna(subset = pheno_cov_df.columns)
-                        N = str(analysis_df.shape[0])
+                        print("ERROR: When fitting user-specified model: ", sys.exc_info()[0], "occurred!")
+                        sys.exit()
 
-                        # Convert pandas dataframe to R dataframe
-                        with localconverter(robjects.default_converter + pandas2ri.converter): 
-                            analysis_df_r = robjects.conversion.py2rpy(analysis_df)
+                    # Convert R dataframe back to pandas dataframe
+                    with localconverter(robjects.default_converter + pandas2ri.converter):
+                        result_df_pd = robjects.conversion.rpy2py(result_df_r)
 
-                        # Call the user-specified R function to perform the association test
-                        try:
-                            result_df_r = model_to_fit(analysis_df_r)
-                        except:
-                            print("ERROR: When fitting user-specified model: ", 
-                                sys.exc_info()[0], 
-                                "occurred!")
-                            sys.exit()
-
-                        # Convert R dataframe back to pandas dataframe
-                        with localconverter(robjects.default_converter + pandas2ri.converter):
-                            result_df_pd = robjects.conversion.rpy2py(result_df_r)
-
-                        out_fh.write(chromosome + " ")
-                        out_fh.write(position + " ")
-                        out_fh.write(rsId + " ")
-                        out_fh.write(refAllele + " ")
-                        out_fh.write(altAllele + " ")
-                        out_fh.write(result_df_pd.estimate.to_string(index = False) + " ")
-                        out_fh.write(result_df_pd.se.to_string(index = False) + " ")
-                        out_fh.write(result_df_pd.p.to_string(index = False) + " ")
-                        out_fh.write(N + "\n")
-                        linesProcessed += 1
-
-                    if(linesProcessed > 0 and linesProcessed % 100 == 0):
-                        print ("PROGRESS: Processed ", linesProcessed, " ", "variants at ", datetime.now())
-            out_fh.close()
+                    chunk_result += chromosome + " "
+                    chunk_result += position + " "
+                    chunk_result += rsId + " "
+                    chunk_result += refAllele + " "
+                    chunk_result += altAllele + " "
+                    chunk_result += result_df_pd.estimate.to_string(index = False) + " "
+                    chunk_result += result_df_pd.se.to_string(index = False) + " "
+                    chunk_result += result_df_pd.p.to_string(index = False) + " "
+                    chunk_result += N + "\n"
+        vcf_file.close()
+        return chunk_result
       
 if __name__ == "__main__":
+    freeze_support()
     main()
